507c4a87024f4316e194b6d74c780febc2794653
p.stepanov <>
2017-11-17 18:13:07 +1000

    Initial commit

---
+++ src/main/java/tasks/Task_1.java
@@ -0,0 +1,42 @@
+package tasks;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        say(Phrase.HELLO_WORLD);
+        Say say = new Say(Phrase.WHATSUP);
+        say(say.getPhrase());
+        say.setPhrase(Phrase.GOODBY_WORLD);
+        say(say.getPhrase());
+    }
+
+    static void say(Phrase phrase) {
+        System.out.println(phrase);
+    }
+
+    enum Phrase {
+        HELLO_WORLD, GOODBY_WORLD, WHATSUP
+    }
+
+    static class Say {
+        String str;
+        static String str2;
+
+        Say(Phrase phrase) {
+            this.phrase = phrase;
+        }
+
+        public Phrase phrase;
+
+        Phrase getPhrase() {
+            return phrase;
+        }
+
+        void setPhrase(Phrase phrase) {
+            this.phrase = phrase;
+        }
+    }
+
+    class Song {
+        String str;
+    }
+}
---
+++ src/main/java/tasks/Task_2_Data_Structure.java
@@ -0,0 +1,14 @@
+package tasks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task_2_Data_Structure {
+    public static void main(String[] args) {
+
+    }
+
+    Map map = new HashMap<String, String>();
+
+
+}
