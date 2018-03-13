f161af0535283e245d8e56dfb47517e384d1e171
Petr Stepanov <p.stepanov@cy.cardpay.com>
2018-03-13 15:17:48 +1000

    Excercise 4.

---
+++ src/main/java/excercises/Excercise_4.java
@@ -0,0 +1,27 @@
+package excercises;
+
+import java.util.Scanner;
+
+public class Excercise_4 {
+
+    /**
+     *
+     * */
+
+    public static void main(String[] args) {
+
+        double[] array = {2.04, 5.45, 7.12,2.04, 5.45, 7.12,2.04, 5.45, 7.12, 10.1};
+
+        System.out.println("abs = " + abs(array));
+    }
+
+    static double abs (double[] array){
+        double sum = 0;
+
+        for(double a: array){
+            sum += a;
+        }
+
+        return sum/array.length;
+    }
+}
