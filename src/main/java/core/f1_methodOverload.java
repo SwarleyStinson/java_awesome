0181939919ba567325c2d2280cc2304e137265d5
Petr Stepanov <p.stepanov@cy.cardpay.com>
2019-01-25 18:22:19 +1000

    Simple overloading.

---
+++ src/main/java/core/f1_methodOverload.java
@@ -0,0 +1,17 @@
+package core;
+
+public class f1_methodOverload {
+    // простая перегрузка метода
+
+    // разные типы параметров
+    // количество параметрво
+    // порядок параметров
+
+    void m(int i, String s) {
+
+    }
+
+    void m(String s, int i) {
+
+    }
+}
