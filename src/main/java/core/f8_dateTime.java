d0300db395a6a9e04a7fe30d20f874fd59854bf8
Petr Stepanov <p.stepanov@cy.cardpay.com>
2019-01-29 18:24:30 +1000

    Дата и время.

---
+++ src/main/java/core/f8_dateTime.java
@@ -0,0 +1,10 @@
+package core;
+
+import java.util.Date;
+
+public class f8_dateTime {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
