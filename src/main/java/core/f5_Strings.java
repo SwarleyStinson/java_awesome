5f3570ffae2d2e08bf2febad759db21b79796a32
Petr Stepanov <p.stepanov@cy.cardpay.com>
2019-01-29 18:04:19 +1000

    Совсем немного про работу с файловой системой.

--- src/main/java/core/f5_Strings.java
+++ src/main/java/core/f5_Strings.java
@@ -1,6 +1,8 @@
 package core;
 
-public class f_Strings {
+import java.util.Random;
+
+public class f5_Strings {
     static String string = "The new String";
 
     /**
@@ -13,6 +15,20 @@ public class f_Strings {
         System.out.println(str.trim());
         System.out.println(str.endsWith(" "));
         System.out.println(str.trim().endsWith(" "));
+
+        /** изменение String = создание новой строки
+         * StringBuilder   или   StringBuffer
+         * */
+
+        Random random = new Random();
+
+        long begin = System.currentTimeMillis();
+        StringBuilder builder = new StringBuilder("string");
+        for (int i = 0; i < 1_000_000; i++) {
+            builder.replace(1, 2, String.valueOf(random.nextInt()));
+//            str = "String" + random.nextInt();
+        }
+        System.out.println(System.currentTimeMillis() - begin);
 
     }
 
@@ -27,6 +43,6 @@ public class f_Strings {
     void stringTest() {
         System.out.println(string.charAt(5));
         System.out.println(String.valueOf(25));
-        System.out.println(String.format("Число %n", 25));
+        System.out.println(String.format("Число %d", 25));
     }
 }
---
+++ src/main/java/core/f6_fileSystem.java
@@ -0,0 +1,12 @@
+package core;
+
+import java.io.*;
+
+public class f6_fileSystem {
+    File file;
+    FileWriter filWriter;
+    FileReader fileReader;
+
+    BufferedReader bufReader;
+    BufferedWriter buffWriter;
+}
