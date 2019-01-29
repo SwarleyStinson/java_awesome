f4ac4bc101a8ca7578ea7c3f9b0c153554dea823
Petr Stepanov <p.stepanov@cy.cardpay.com>
2019-01-29 16:59:18 +1000

    Добавил немного знаний о массиве.

--- src/main/java/core/f3_Array.java
+++ src/main/java/core/f3_Array.java
@@ -5,24 +5,40 @@ import java.util.Arrays;
 import java.util.List;
 
 public class f3_Array {
-    /** DECLARATION */
+    /**
+     * DECLARATION
+     */
     int[] array;  // предпочтительный способ
-    int array2[]; // тоже правильный, но не предпочтительный.
+    int array2[]; // тоже правильный, но НЕ предпочтительный.
 
-    /** INITIALIZATION */
+    /**
+     * INITIALIZATION
+     */
     static int[] array1 = new int[100];
     static int[] array3 = {1, 3, 2, 5};
+    static int[] array4 = new int[]{1, 3, 9, 5};
 
     public static void main(String[] args) {
         /** STATIC METHODS */
         List<Integer> list = Arrays.asList(1, 2, 3); // преобразует аргументы в List<T>
 
-        System.out.println(Arrays.equals(array3, new int[10])); // сравнение двух массивов
+        /** СРАВНЕНИЕ двух массивов */
+        System.out.println(Arrays.equals(array3, new int[]{1, 3, 9, 5}));
+        System.out.println(Arrays.equals(array3, new int[]{1, 2, 3, 0}));
+        System.out.println(Arrays.equals(array3, new int[]{1, 2}));
+
+
         System.out.println(Arrays.binarySearch(array3, 3));
-        System.out.println(array3.toString());
-        Arrays.sort(array3);  //  сортирует массив
+        System.out.println(Arrays.toString(array3));
+
+        /** Сортировка массива */
+        Arrays.sort(array3);
+
+        System.out.println(Arrays.toString(array3));
+        System.out.println(array3.length);
+
+        /** Проверка массива на наличие конкретного значения*/
+        System.out.println(Arrays.asList(array3).contains(1));
         
-        System.out.println(array3.toString());
-        System.out.println(array1.length);
     }
 }
