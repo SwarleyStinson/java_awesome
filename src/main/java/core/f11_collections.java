17e33d6d9960390c73d659b71cf64e6b24a68c78
Petr Stepanov <p.stepanov@cy.cardpay.com>
2019-01-30 15:45:45 +1000

    Collections.

---
+++ src/main/java/core/f11_collections.java
@@ -0,0 +1,50 @@
+package core;
+
+import java.util.*;
+
+public class f11_collections {
+    public static void main(String[] args) {
+        /** интерфейс Collection
+         * .add()
+         * .iterator()
+         * */
+        Collection collection = new ArrayList();
+        collection.add(1);
+        collection.add(2);
+        collection.add(3);
+
+        Iterator iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        for (Object j : collection) {
+            System.out.println(j);
+        }
+
+        /** интерфейс List
+         * *              и его реализации ArrayList  и  LinkedList
+         * .set()
+         * .get()
+         * .indexOf()
+         * */
+        ArrayList list = new ArrayList();
+        LinkedList linkedList = new LinkedList();
+
+        /** интерфейс Set
+         * *             и его реализации HashSet
+         * *                              TreeSet
+         * *                              EnumSet
+         * *                              LinkedHashSet
+         * */
+        Set set = new HashSet();
+
+        /** интерфейс Queue
+         * *               и его реализации ArrayDeque
+         * *                                PriorityQueue
+         * */
+
+        Collections collections;
+        Arrays arrays;
+    }
+}
