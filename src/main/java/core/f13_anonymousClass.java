cc8a3cd6e37167c6253b016fcbf87867a1c06e34
Petr Stepanov <p.stepanov@cy.cardpay.com>
2019-01-31 17:56:02 +1000

    Cloning.

---
+++ src/main/java/core/f13_anonymousClass.java
@@ -0,0 +1,35 @@
+package core;
+
+class Popcorn {
+    void doSomething() {
+        System.out.println("popcorn");
+    }
+}
+
+public class f13_anonymousClass {
+    public static void main(String[] args) {
+        /**
+         *  Переопределить при создании объекта.
+         */
+        Popcorn popcorn = new Popcorn() {
+            @Override
+            void doSomething() {
+                System.out.println("override popcorn");
+            }
+        };
+        popcorn.doSomething();
+
+        /** При передаче объекта в качестве параметра.*/
+        method(new Popcorn() {
+            @Override
+            void doSomething() {
+                System.out.println("override in method parameters !!!");
+            }
+        });
+
+    }
+
+    static void method(Popcorn popcorn) {
+        popcorn.doSomething();
+    }
+}
