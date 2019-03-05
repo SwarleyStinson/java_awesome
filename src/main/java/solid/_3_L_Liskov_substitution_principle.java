9c4801b6a1dd2aab35b390ecc5b36df10c93ec36
petr <fromuralwithlove@gmail.com>
2019-03-05 20:13:25 +1000

    SOLID patterns.

---
+++ src/main/java/solid/_3_L_Liskov_substitution_principle.java
@@ -0,0 +1,11 @@
+package solid;
+
+public class _3_L_Liskov_substitution_principle {
+    /**
+     * При наследовании мы не должны изменять поведение родителя.
+     * */
+
+    public static void main(String[] args) {
+
+    }
+}
