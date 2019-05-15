b4dd38f4f1629b35d061dfb87888ed9cc277e5c6
p.stepanov <p.stepanov@cy.cardpay.com>
2019-05-15 16:59:54 +1000

    stream

--- src/main/java/core/java8core/stream.java
+++ src/main/java/core/java8core/stream.java
@@ -1,24 +1,21 @@
 package core.java8core;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class stream {
     public static void main(String[] args) {
-        List<A> list = new ArrayList<>();
-        list.add(new A("field1", new ArrayList<>()));
-        list.add(new A("field2", null));
+        Map<String, String[]> map = new HashMap<>();
+        map.put("param1", new String[]{"value1"});
+        map.put("param2", new String[]{"value2"});
+        map.put("param3", new String[]{"value3"});
 
-        List<A> collect = list.stream()
-                .filter(t -> t.field.startsWith("field1"))
-                .filter(t -> {
-                    System.out.println(t.field);
-                    t.list.size();
-                    t.field.equals("field2");
-                    return true;
-                })
-                .collect(Collectors.toList());
+        Map<String, String> result = map.entrySet().stream()
+                .collect(Collectors.toMap(x -> x.getKey(), x -> (x.getValue())[0]));
+        System.out.println(1);
     }
 }
 
