c5c4aef34e1d0786335ebf1fbf3b9f8159c11111
p.stepanov <p.stepanov@cy.cardpay.com>
2019-07-01 17:17:10 +1000

    bla bla

--- src/main/java/core/datastructure/MapToList.java
+++ src/main/java/core/datastructure/MapToList.java
@@ -11,9 +11,10 @@ public class MapToList {
         map.put("key1", "value1");
         map.put("key2", "value2");
 
-        List<String> list = new ArrayList<>();
-
         /** .VALUES()  */
+        /** 1 case */
+        List<String> list = new ArrayList<>(map.values());
+        /** 2 case */
         list.addAll(map.values());
 
         System.out.println(list);
