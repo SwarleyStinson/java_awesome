0bdfe3cfe388f66af46ce96969377ea55e1a4e32
petr <fromuralwithlove@gmail.com>
2020-03-30 08:20:56 +1000

    predicate

---
+++ src/main/java/java8core/FunctionClass.java
@@ -0,0 +1,17 @@
+package java8core;
+
+import com.google.common.base.Function;
+
+public class FunctionClass {
+    public static void main(String[] args) {
+        Function<Integer, String> m1 = FunctionClass::staticMethod;
+
+        System.out.println(m1.apply(1));
+    }
+
+    public static String staticMethod(int i) {
+        return i == 0
+                ? "12345"
+                : "54321";
+    }
+}
